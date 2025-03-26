999252c996f044f3ed048207017fd114c8fa4e89
Edward Ashenhurst <eashenhurst@geologicai.com>
2025-03-26 13:48:21 -0600

    adding pickle file try 2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="lidar_processor", 
     version="0.1.0",
     packages=find_packages(),
-    package_data={"lidar_processor": ["lidar_processing/cluster_kd_tree.pkl"]},
+    package_data={"lidar_processor": ["cluster_kd_tree.pkl"]},
     install_requires=[
         "numpy",
         "cupy-cuda12x",  # Adjust based on CUDA version
