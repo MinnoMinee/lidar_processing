3971e3fb67f1dd6b45ac5351f0fea7f3769c21cb
Edward Ashenhurst <eashenhurst@geologicai.com>
2025-03-26 13:55:20 -0600

    attempt 3 pkl file

--- setup.py
+++ setup.py
@@ -3,14 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="lidar_processor", 
     version="0.1.0",
-    packages=find_packages(),
-    package_data={"lidar_processor": ["cluster_kd_tree.pkl"]},
+    packages=find_packages(include =["lidar_processing","lidar_processing.*"]),
+    package_data={"folder2": ["*.pkl"]},
+    include_package_data = True,
     install_requires=[
-        "numpy",
-        "cupy-cuda12x",  # Adjust based on CUDA version
-        "scipy",
-        "matplotlib",
-        "opencv-python",
-        "open3d",
+        line.strip() for line in open("requirements.txt").readlines() if line.strip()
     ],
 )
